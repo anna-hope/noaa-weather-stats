4555bc53499b4b5f534e612090632a57bfdd8d51
anna.melnikov <anna.me@hey.com>
2021-02-14 10:40:18 -0600

    Add functions to compute stats for all NOAA files in parallel and create combined summary stats

--- noaa_tools/process_data.py
+++ noaa_tools/process_data.py
@@ -1,12 +1,19 @@
 from datetime import datetime as dt, timedelta
-from functools import partial
-import gzip
-import io
 import os
 from pathlib import Path
+from typing import Dict
 
 from joblib import Parallel, delayed
 import pandas as pd
+
+# (from NOAA) The five core elements are:
+# PRCP = Precipitation (tenths of mm)
+# SNOW = Snowfall (mm)
+# SNWD = Snow depth (mm)
+# TMAX = Maximum temperature (tenths of degrees C)
+# TMIN = Minimum temperature (tenths of degrees C)
+
+core_keys = {"PRCP", "SNOW", "SNWD", "TMAX", "TMIN"}
 
 
 def read_noaa_data(data_file: Path) -> pd.DataFrame:
@@ -32,6 +39,10 @@ def read_noaa_data(data_file: Path) -> pd.DataFrame:
     common_keys = key_counts[key_counts > 0.05]
     common_keys = common_keys.index
 
+    # but the core keys should always be there
+    if not core_keys.issubset(common_keys):
+        raise ValueError(f"The dataframe is missing the core keys {core_keys}")
+
     df = df.loc[df.element_key.isin(common_keys)]
 
     # pivot the dataframe so that the values of element_key are columns
@@ -39,3 +50,39 @@ def read_noaa_data(data_file: Path) -> pd.DataFrame:
         index=["station_id", "date"], columns="element_key", values="element_value"
     )
     return df
+
+
+def compute_means(df: pd.DataFrame) -> pd.DataFrame:
+    # get the year
+    # the dataframe's index is a MultiIndex in the format
+    # (station_id, yyyymmdd)
+    df_first_timestamp = df.index[0][1]
+    df_year = dt.strptime(str(df_first_timestamp), "%Y%m%d").year
+
+    summary_stats: Dict[str, float] = df.mean().to_dict()
+    for key, value in summary_stats.items():
+        # temperatures are given in tenths of degrees C
+        # so to convert them to regular C we need to /10
+        if key.startswith("T"):
+            summary_stats[key] = value / 10
+
+    df_summary = pd.DataFrame(summary_stats, index=[df_year])
+    return df_summary
+
+
+def get_means_df_file(data_file: Path) -> pd.DataFrame:
+    df = read_noaa_data(data_file)
+    df_means = compute_means(df)
+    return df_means
+
+
+def get_means_all_noaa_files(data_dir: Path = Path("data")) -> pd.DataFrame:
+    data_files = [item for item in data_dir.iterdir() if ".csv" in item.suffixes]
+
+    # don't overload the machine -- only use half + 1 of all available CPUs
+    max_cpus = os.cpu_count() // 2 + 1
+    dfs_means = Parallel(n_jobs=max_cpus)(
+        delayed(get_means_df_file)(data_file) for data_file in data_files
+    )
+    df_means_combined = pd.concat(dfs_means)
+    return df_means_combined
