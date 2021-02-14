21c55f04ce8c8bcfc2bf79609658f8d18efaa0f0
anna.melnikov <anna.me@hey.com>
2021-02-14 11:36:02 -0600

    add driver script

--- runme.py
+++ runme.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 from argparse import ArgumentParser
 from datetime import datetime as dt, timedelta
 
-from noaa_tools import get_data
+from noaa_tools import get_data, process_data
 
 cur_year = dt.now().year
 
@@ -19,3 +21,15 @@ arg_parser.add_argument(
     help="last year for which to get the data",
 )
 args = arg_parser.parse_args()
+
+
+def main():
+    print("Getting the data...")
+    data_paths = get_data.download_historical_data(args.year_start, args.year_end)
+    print("Computing means for every year")
+    df_means_years = process_data.get_means_noaa_files(data_paths)
+    print(df_means_years.idxmax())
+
+
+if __name__ == "__main__":
+    main()
